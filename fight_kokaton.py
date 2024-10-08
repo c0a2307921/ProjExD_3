5ca29dff270bd71b2773e89d52109b4917e7daec
kou <c0a2307921@edu.teu.ac.jp>
2024-10-08 17:48:07 +0900

    修正 #1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -82,7 +82,7 @@ class Bird:
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
             self.img = __class__.imgs[tuple(sum_mv)]
-        screen.blit(self.img, self.rct)
+        screen.blit(self.img,self.rct)
 
 
 class Beam:
